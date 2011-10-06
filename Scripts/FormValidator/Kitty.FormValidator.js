32d3f2e0be20ee2c76bb28a4494dbdd08ac3bb84
Adam Silver <adambsilver@gmail.com>
2011-10-06 16:47:11 +0100

    further specs

--- Scripts/FormValidator/Kitty.FormValidator.js
+++ Scripts/FormValidator/Kitty.FormValidator.js
@@ -1,13 +1,24 @@
 var Kitty = Kitty || {};
-Kitty.FormValidator = function(container) {
+Kitty.FormValidator = function(form) {
+	this.form = form;
 	var invalidContainerMessage = "Invalid container. Must be a jquery object with form element.";
-	if(typeof container == "undefined") {
+	if(typeof form == "undefined") {
 		throw invalidContainerMessage;
 	}
-	if(!(container instanceof jQuery)) {
+	if(!(form instanceof jQuery)) {
 		throw invalidContainerMessage;
 	}
-	if(!container.is("form")) {
+	if(!form.is("form")) {
 		throw invalidContainerMessage;
+	}
+}
+Kitty.FormValidator.prototype.addValidator = function(fieldName, rules) {
+	var field = this.form.find("[name="+fieldName+"]");
+	// if field does not exist
+	if(!field.length) {
+		throw "Invalid form field.";
+	}
+	if(!rules) {
+		throw "Invalid rules. Must provide be an array of rules (at least 1).";
 	}
 }
--- Specs/FormValidator/Spec.Kitty.FormValidator.js
+++ Specs/FormValidator/Spec.Kitty.FormValidator.js
@@ -28,6 +28,40 @@ describe("Form Validator", function() {
         }).toThrow("Invalid container. Must be a jquery object with form element.");
       });
     });
+    describe("With a valid jQuery object containing a form", function() {
+      it("doesn't throw an error", function() {
+         expect(function() {
+          new Kitty.FormValidator($("form"));
+        }).not.toThrow();
+      });
+    });
+  });
+
+  describe("Adding a validator", function() {
+    describe("With non-existant form field", function() {
+      it("throws an error", function() {
+        var formValidator = new Kitty.FormValidator($("form"));        
+        expect(function() {
+          formValidator.addValidator();
+        }).toThrow("Invalid form field.");
+      });
+    });
+    describe("With no rules", function() {
+      it("throws an error", function() {
+        var formValidator = new Kitty.FormValidator($("form"));        
+        expect(function() {
+          formValidator.addValidator("username");
+        }).toThrow("Invalid rules. Must provide be an array of rules (at least 1).");
+      });
+    });
+    describe("With valid params", function() {
+      it("doesn't throw an error", function() {
+        var formValidator = new Kitty.FormValidator($("form"));        
+        expect(function() {
+          //formValidator.addValidator("username");
+        }).not.toThrow();  
+      });
+    });
   });
   
 });
