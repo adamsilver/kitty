3ce6b6d64c761fa52b77c71f9418b5a062f7c5e6
Adam Silver <adambsilver@gmail.com>
2011-11-03 09:20:59 +0000

    specs for contextual submit

--- Scripts/ContextualSubmit/Kitty.ContextualSubmit.js
+++ Scripts/ContextualSubmit/Kitty.ContextualSubmit.js
@@ -1,4 +1,21 @@
 var Kitty = Kitty || {};
 Kitty.ContextualSubmit = function(field, submitButton) {
-	
+	this.form = field.parents("form");
+	this.field = field;
+	this.submitButton = submitButton;
+	this.field.bind("focus", $.proxy(this, "handleField_onFocus"));
+	this.field.bind("blur", $.proxy(this, "handleField_onBlur"));
+	this.clonedSubmitButton = this.submitButton.clone();
+	this.clonedSubmitButton.attr("id", "");
+	this.clonedSubmitButton.addClass("offScreen");
+}
+Kitty.ContextualSubmit.prototype.handleField_onFocus = function(e) {
+	this.form.prepend(this.clonedSubmitButton);
+}
+Kitty.ContextualSubmit.prototype.handleField_onBlur = function(e) {
+	this.clonedSubmitButton.remove();
+}
+Kitty.ContextualSubmit.prototype.destroy = function() {
+	this.field.unbind("focus", this.handleField_onFocus);
+	this.field.unbind("blur", this.handleField_onBlur);
 }
--- Specs/ContextualSubmit/Spec.Kitty.ContextualSubmit.js
+++ Specs/ContextualSubmit/Spec.Kitty.ContextualSubmit.js
@@ -8,26 +8,47 @@ describe("Contextual Submit", function() {
 
 	beforeEach(function() {
 		jasmine.getFixtures().load('Spec.Kitty.ContextualSubmit.Fixture1.html');
-		field = $("#formFixture").find("input[type=text]");
-		submitButton = $("#formFixture").find("input[type=submit]");
+		field = $("#someField");
+		submitButton = $("#submitButton");
+		contextualSubmit = new Kitty.ContextualSubmit(field, submitButton);
 	});
 
-	describe("focusing on field", function() {
-
-		it("prepends clone to the parent form", function() {
-			contextualSubmit = new Kitty.ContextualSubmit(field, submitButton);
+	describe("Focusing on field", function() {
+		var clonedSubmitButton = null;
+		beforeEach(function() {
 			field[0].focus();
-			expect($("#formFixture").find(">input").attr("type")).toBe(submitButton.attr("type"));
+			clonedSubmitButton = $("#someForm").find(">input");
 		});
-
+		it("Prepends cloned submit button to the parent form", function() {
+			expect(clonedSubmitButton.attr("type")).toBe(submitButton.attr("type"));
+		});
+		it("Removes id from cloned submit button", function() {
+			expect(clonedSubmitButton.attr("id")).toBe("");
+		});
+		it("Hides the cloned submit button off screen", function() {
+			expect(clonedSubmitButton.hasClass("offScreen")).toBe(true);
+		});
 	});
 
-	describe("blurring from field", function() {
-
-		it("removes clone", function() {
-
+	describe("Blurring from field", function() {
+		beforeEach(function() {
+			field[0].focus(); // setup state
+			field[0].blur(); // exercise
 		});
+		it("Removes cloned submit button", function() {
+			var firstInput = $("#someForm").find(">input");
+			expect(firstInput.attr("name")).not.toBe(submitButton.attr("name"));
+		});
+	});
 
+	describe("Destroying the contextual submit", function() {
+		it("Removes the events", function() {
+			spyOn($.fn, "unbind");
+			contextualSubmit.destroy();
+			expect($.fn.unbind.mostRecentCall.object).toBe(field);
+			expect($.fn.unbind).toHaveBeenCalledWith("focus", contextualSubmit.handleField_onFocus);
+			expect($.fn.unbind).toHaveBeenCalledWith("blur", contextualSubmit.handleField_onBlur);
+		});
 	});
 
 });
