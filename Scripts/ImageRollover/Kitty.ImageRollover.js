302031e5e95bff72bc56e0e8ec9074412b4d8b72
Adam Silver <adambsilver@gmail.com>
2011-11-04 16:45:46 +0000

    Added spec and some implementation of image rollover

---
+++ Scripts/ImageRollover/Kitty.ImageRollover.js
@@ -0,0 +1,19 @@
+var Kitty = Kitty || {};
+Kitty.ImageRollover = function(image, rolloverImageSource) {
+	this.rolloverImageSource = rolloverImageSource;
+	this.preloadedImage = new Image();
+	this.preloadedImage.src = rolloverImageSource;
+	this.image = image;
+	this.originalImageSource = this.image.attr("src");
+	this.image.on("mouseover", $.proxy(this, "handleImage_onMouseOver"));
+	this.image.on("mouseout", $.proxy(this, "handleImage_onMouseOut"));
+}
+Kitty.ImageRollover.prototype.handleImage_onMouseOver = function(e) {
+	this.image.attr("src", this.rolloverImageSource);
+}
+Kitty.ImageRollover.prototype.handleImage_onMouseOut = function(e) {
+	this.image.attr("src", this.originalImageSource);
+}
+Kitty.ImageRollover.prototype.destroy = function() {
+	
+}
--- Specs/ImageRollover/Spec.Kitty.ImageRollover.js
+++ Specs/ImageRollover/Spec.Kitty.ImageRollover.js
@@ -1,5 +1,12 @@
 describe("Image Rollover", function() {
 
+	jasmine.getFixtures().fixturesPath = '.';
+
+	var imageRollover = null;
+	beforeEach(function() {
+		jasmine.getFixtures().load('Spec.Kitty.ImageRollover.Fixture1.html');
+	});
+
 	describe("Creating a new image rollover", function() {
 		it("Preloads the new image into memory", function() {
 			spyOn(window, "Image");
@@ -13,18 +20,27 @@ describe("Image Rollover", function() {
 
 	describe("Mousing over the image", function() {
 		it("Updates the source of the image to the new image source", function() {
-			
+			var img = $("img");
+			var imageRollover = new Kitty.ImageRollover(img, "newImageSource");
+			img.trigger("mouseover");
+			expect(img.attr("src")).toBe("newImageSource");
 		});
 	});
 
 	describe("Mousing out of the image", function() {
 		it("Updates the source of the image to the original image source", function() {
-			
+			var img = $("img");
+			var originalSource = img.attr("src");
+			var imageRollover = new Kitty.ImageRollover(img, "newImageSource");
+			img.trigger("mouseover");
+			img.trigger("mouseout");
+			expect(img.attr("src")).toBe(originalSource);
 		});
 	});
 
 	describe("Destroying the image rollover", function() {
 		it("Removes the events", function() {
+			var img = $("img");
 			var imageRollover = new Kitty.ImageRollover(img, "newImageSource");
 			imageRollover.destroy();
 		});
