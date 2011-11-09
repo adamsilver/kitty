ca706bf0e2df36aa607ab7b0ec1dc946efa6e1e8
Adam Silver <adambsilver@gmail.com>
2011-11-09 14:53:12 +0000

    spec for tooltip

---
+++ Specs/ToolTip/Spec.Kitty.Tooltip.js
@@ -0,0 +1,13 @@
+describe("Tooltip", function() {
+	describe("Activing a tooltip", function() {
+		describe("Via keyboard focus");
+		describe("Via mouseenter");
+	});
+	describe("Deactivating a tooltip", function() {
+		describe("Via keyboard blur");
+		describe("Via mouseleave");
+	});
+	describe("Destroying tooltip", function() {
+		
+	});
+});
