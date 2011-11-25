8a5eb8fbb4b9e2df557eb62e12b7eb7e7642a61a
Adam Silver <adambsilver@gmail.com>
2011-11-25 14:17:28 +0000

    specs for star rating control

--- Scripts/StarRating/Kitty.StarRating.js
+++ Scripts/StarRating/Kitty.StarRating.js
@@ -2,13 +2,16 @@ var Kitty = Kitty || {};
 Kitty.StarRating = function(radioContainers) {
 	this.radios = radioContainers.find("input[type=radio]");
 	this.radios.addClass("offScreen");
-	this.currentRating = this.radios.filter(":checked").val();
+	this.currentRating = this.radios.filter(":checked").val() || null;	
 	this.labels = radioContainers.find("label");
 	this.labels.on("mouseover", $.proxy(this, "handleLabel_onMouseover"));
 	this.labels.on("mouseout", $.proxy(this, "handleLabel_onMouseout"));
 	this.radios.on("focus", $.proxy(this, "handleRadio_onFocus"));
 	this.radios.on("blur", $.proxy(this, "handleRadio_onBlur"));
 	this.radios.on("change", $.proxy(this, "handleRadio_onChange"));
+	if(this.currentRating) {
+		this.highlightStars(this.currentRating);
+	}
 }
 Kitty.StarRating.prototype.handleLabel_onMouseover = function(e) {
 	var relatedRadio = $(e.target).parents(".radio").find("input[type=radio]");
@@ -16,8 +19,9 @@ Kitty.StarRating.prototype.handleLabel_onMouseover = function(e) {
 	this.highlightStars(rating);
 }
 Kitty.StarRating.prototype.highlightStars = function(rating) {
+	rating = parseInt(rating,10)-1;
 	for(var i = 0; i < this.labels.length; i++) {
-		if(rating > i) {
+		if(rating >= i) {
 			this.labels.filter(":eq("+i+")").addClass("highlight");
 		}
 		else {
@@ -35,5 +39,6 @@ Kitty.StarRating.prototype.handleRadio_onBlur = function(e) {
 	this.highlightStars(0);
 }
 Kitty.StarRating.prototype.handleRadio_onChange = function(e) {
-	this.highlightStars($(e.target).val());
+	this.currentRating = $(e.target).val();
+	this.highlightStars(this.currentRating);
 }
--- Specs/StarRating/Spec.Kitty.StarRating.js
+++ Specs/StarRating/Spec.Kitty.StarRating.js
@@ -6,30 +6,66 @@ describe("Star Rating", function() {
 	var radioContainers = null;
 	var radios = null;
 	var labels = null;
+	var cssHighlightedClass = "highlight";
 
-	beforeEach(function() {
+	// has checked radio
+	function setupFixture1() {
 		jasmine.getFixtures().load('Spec.Kitty.StarRating.Fixture1.html');
+		commonSetup();
+	}
+
+	// doesn't have checked radio
+	function setupFixture2() {
+		jasmine.getFixtures().load('Spec.Kitty.StarRating.Fixture2.html');
+		commonSetup();
+	}
+
+	function commonSetup() {
 		radioContainers = $(".radio");
 		radios = $(".radio input");
 		labels = $(".radio label");
 		starRating = new Kitty.StarRating(radioContainers);
-	});
+	}
 
 	describe("Creating a new Star Rating control", function() {
+
+		beforeEach(setupFixture1);
+
 		it("Hides the radio inputs", function(){
 			for(var i = 0; i < radios.length; i++) {
 				expect(radios.filter(":eq("+i+")")).toHaveClass("offScreen");
 			}
 		});
-		it("Stores the current rating from the currently selected radio", function() {
-			expect(starRating.currentRating).toBe(radios.filter(":checked").val());
+
+		describe("That has a checked radio", function() {
+			it("Stores the current rating from the currently selected radio", function() {
+				expect(starRating.currentRating).toBe(radios.filter(":checked").val());
+			});
+			it("Highlights the labels up until that rating", function() {
+				expect(labels.filter(":eq(0)")).toHaveClass(cssHighlightedClass)
+				expect(labels.filter(":eq(1)")).toHaveClass(cssHighlightedClass)
+			});
+		});
+
+		describe("That doesn't have a checked radio", function() {
+			beforeEach(setupFixture2);
+			it("Stores the current rating from the currently selected radio", function() {
+				expect(starRating.currentRating).toBe(null);
+			});
 		});
 	});
+
+
+
+
 	describe("Mouse over a rating label (of 3)", function() {
+
+		beforeEach(setupFixture1);
+
 		it("Highlights the first 3 stars", function() {
 			labels.filter(":eq(2)").trigger("mouseover");
-			for(var i = 0; i < 3; i++) {
-				expect(labels.filter(":eq("+i+")")).toHaveClass("highlight");
+			for(var i = 0; i < 2; i++) {
+				expect(labels.filter(":eq("+i+")")).toHaveClass(cssHighlightedClass);
 			}
 		});
 
@@ -37,25 +73,27 @@ describe("Star Rating", function() {
 			it("Unhighlights the stars after the first 2 stars", function() {
 				labels.filter(":eq(2)").trigger("mouseover");
 				labels.filter(":eq(1)").trigger("mouseover");
-				expect(labels.filter(":eq(2)")).not.toHaveClass("highlight");
+				expect(labels.filter(":eq(2)")).not.toHaveClass(cssHighlightedClass);
 			});
 		});
 	});
 
-	describe("Mouse over in element of rating label (of 3)", function() {
+	describe("Mouse over an element inside of rating label (of 3)", function() {
+		beforeEach(setupFixture1);
 		it("Highlights the first 3 stars", function() {
 			labels.filter(":eq(2)").find("span").trigger("mouseover");
-			for(var i = 0; i < 3; i++) {
-				expect(labels.filter(":eq("+i+")")).toHaveClass("highlight");
+			for(var i = 0; i < 2; i++) {
+				expect(labels.filter(":eq("+i+")")).toHaveClass(cssHighlightedClass);
 			}
 		});
 	});
 
 	describe("Focus on a rating label (of 3)", function() {
+		beforeEach(setupFixture1);
 		it("Highlights the first 3 stars", function() {
 			radios.filter(":eq(2)").trigger("focus");
-			for(var i = 0; i < 3; i++) {
-				expect(labels.filter(":eq("+i+")")).toHaveClass("highlight");
+			for(var i = 0; i < 2; i++) {
+				expect(labels.filter(":eq("+i+")")).toHaveClass(cssHighlightedClass);
 			}
 		});
 
@@ -63,54 +101,60 @@ describe("Star Rating", function() {
 			it("Unhighlights the stars after the first 2 stars", function() {
 				radios.filter(":eq(2)").trigger("focus");
 				radios.filter(":eq(1)").trigger("focus");
-				expect(labels.filter(":eq(2)")).not.toHaveClass("highlight");
+				expect(labels.filter(":eq(2)")).not.toHaveClass(cssHighlightedClass);
 			});
 		});
 	});
 	describe("Mouse leaving a rating label", function() {
-
+		beforeEach(setupFixture2);
 		describe("No selected rating", function() {
 			it("Unhighlights the stars", function() {
 				labels.filter(":eq(2)").trigger("mouseover"); // setup state
 				labels.filter(":eq(2)").trigger("mouseout");
 				for(var i = 0; i < 3; i++) {
-					expect(labels.filter(":eq("+i+")")).not.toHaveClass("highlight");
+					expect(labels.filter(":eq("+i+")")).not.toHaveClass(cssHighlightedClass);
 				}
 			});
 		});
 
-		describe("A selected rating of 3", function() {
-			it("Highlights the 3 stars", function() {
-				for(var j = 0; j < radios.length; j++) {
-					
-				}
-				labels.filter(":eq(2)").trigger("mouseover"); // setup state
+		describe("And it has a selected rating of 2", function() {
+			beforeEach(setupFixture1);
+			it("Highlights the 2 stars", function() {
+				labels.filter(":eq(2)").trigger("mouseover");
 				labels.filter(":eq(2)").trigger("mouseout");
-				for(var i = 0; i < 3; i++) {
-					expect(labels.filter(":eq("+i+")")).toHaveClass("highlight");
-				}
+				
+				expect(labels.filter(":eq(0)")).toHaveClass(cssHighlightedClass);
+				expect(labels.filter(":eq(1)")).toHaveClass(cssHighlightedClass);
+				expect(labels.filter(":eq(2)")).not.toHaveClass(cssHighlightedClass);
+
 			});
 		});
 		
 	});
 	describe("Blurring the rating input", function() {
+		beforeEach(setupFixture1);
 		it("Unhighlights the first 3 stars", function() {
 			radios.filter(":eq(2)").trigger("focus"); // setup state
 			radios.filter(":eq(2)").trigger("blur");
-			for(var i = 0; i < 3; i++) {
-				expect(labels.filter(":eq("+i+")")).not.toHaveClass("highlight");
+			for(var i = 0; i < 2; i++) {
+				expect(labels.filter(":eq("+i+")")).not.toHaveClass(cssHighlightedClass);
 			}
 		});
 	});
 	describe("Selecting a rating (rating 3)", function() {
-		it("Persists the chosen rating by highlighting the stars", function() {
+		beforeEach(setupFixture2);
+		it("Persists the chosen rating by highlighting the stars", function() {					
 			radios.filter(":eq(2)").click();
+			labels.filter(":eq(2)").mouseout();
 			for(var i = 0; i < 3; i++) {
-				expect(labels.filter(":eq("+i+")")).toHaveClass("highlight");
+				expect(labels.filter(":eq("+i+")")).toHaveClass(cssHighlightedClass);
 			}
 		});
 		it("Marks the radio as selected", function() {
-			
+			labels.filter(":eq(2)").click();
+			for(var i = 0; i < 3; i++) {
+				expect(labels.filter(":eq("+i+")")).toHaveClass(cssHighlightedClass);
+			}
 		});
 	});
 	describe("Destroying the Star Rating control", function() {
