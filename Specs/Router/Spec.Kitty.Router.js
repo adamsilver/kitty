3d16df80f8886c4a04c39297aad7d12fef4425ad
Adam Silver <adambsilver@gmail.com>
2011-11-28 11:05:27 +0000

    Router spec started

---
+++ Specs/Router/Spec.Kitty.Router.js
@@ -0,0 +1,30 @@
+describe("Kitty.Router", function() {
+	
+	function changeLocation(newLocation) {
+		document.location.hash = newLocation;
+	}
+
+	describe("Reacting to a particular route", function() {
+		it("Fires the callback with url params", function() {
+			var callback = createSpy();
+			var router = new Kitty.Router();
+			router.route("/some/path/:some_param/", callback);
+
+			changeLocation("/some/path/123/");
+			
+			expect(func).toHaveBeenCalled();
+			expect.(func.mostRecentCall.args[0].urlParams.some_param).toBe("123");
+		});
+
+		it("Fires callback with query string params", function() {
+			var callback = createSpy();
+			var router = new Kitty.Router();
+			router.route("/some/path/?some_qs_param", callback);
+			changeLocation("/some/path/?some_qs_param=woop");
+			
+			expect(func).toHaveBeenCalled();
+			expect.(func.mostRecentCall.args[0].queryStringParams.some_qs_param).toBe("woop");
+		});
+	});
+
+});
