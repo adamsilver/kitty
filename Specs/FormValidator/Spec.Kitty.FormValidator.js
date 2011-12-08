af7502df9146651d75f5808f2fd0b9ac868adafb
Adam Silver <adambsilver@gmail.com>
2011-12-08 13:55:58 +0000

    added extra check for param

--- Specs/FormValidator/Spec.Kitty.FormValidator.js
+++ Specs/FormValidator/Spec.Kitty.FormValidator.js
@@ -118,8 +118,10 @@ describe("Form Validator", function() {
       formValidator.addValidator("username", [rules.usernameInvalidWithParams]);
       spyOn(rules.usernameInvalidWithParams, "method");
       formValidator.validate();
+      debugger;
       var args = rules.usernameInvalidWithParams.method.mostRecentCall.args;
       expect(args[1]).toBe(rules.usernameInvalidWithParams.params);
+      expect(args[1].whatever).toBe(rules.usernameInvalidWithParams.params.whatever);
     });
 
     describe("Which contains errors", function() {
