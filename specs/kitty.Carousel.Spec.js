85df6e4b2f0b2a03c1bb571ad72f707c6c6b0dc9
Adam Silver <adambsilver@gmail.com>
2014-08-05 11:36:48 +0100

    added carousel specs, just constructor tested so far

--- specs/kitty.Carousel.Spec.js
+++ specs/kitty.Carousel.Spec.js
@@ -1,69 +1,50 @@
 describe("Carousel", function() {
 
-	var fixture;
-	var originalHtml;
-	var $carousel = null;
+	var carousel;
+    var mockContainer;
+    var mockItemsContainer;
+    var mockItems;
+    var mockItemsWidth;
 
 	beforeEach(function() {
-		fixture = $("#fixture2");
-		originalHtml = fixture.html();
-		$carousel = fixture.find(".carousel");
+		mockContainer = jasmine.createSpyObj('mockContainer', ['find']);
+        mockItemsContainer = jasmine.createSpyObj('mockItemsContainer', ['find', 'width']);
+        mockItems = jasmine.createSpyObj('mockItem', ['width']);
+        mockItems.length = 2;
+        mockItemsWidth = 100;
+
+        mockContainer.width = jasmine.createSpy();
+
+        mockItems.width = jasmine.createSpy().and.returnValue(mockItemsWidth);
+
+        mockContainer.find = jasmine.createSpy().and.callFake(function(arg) {
+            if(arg === 'ul.items') {
+                return mockItemsContainer;
+            }
+        });
+
+        mockItemsContainer.find = jasmine.createSpy().and.callFake(function(arg) {
+            if(arg === 'li.item') {
+                return mockItems;
+            }
+        });
 	});
 
-	afterEach(function() {
-		fixture.html(originalHtml);
-	});
-
-	describe("Creating a new carousel", function() {
-		it("Sets the width of the UL to the total size of all the LI elements.", function() {
-			var carousel = new kitty.Carousel($carousel);
-			var length = $carousel.find("li.item").length;
-			var width = $carousel.find("li.item").width();
-			var totalWidth = width * length;
-			expect($carousel.find("ul.items").width()).toBe(totalWidth);
-		});
-	});
-
-
-	xdescribe("Moving the carousel backwards", function() {
-		it("Moves the carousel backwards.", function(done) {
-			var carousel = new kitty.Carousel($carousel);
-			carousel.moveBackwards();
-			waits(450);
-			runs(function() {
-				expect(carousel.ul.css("left")).toBe("-200px");
-				carousel.moveBackwards();
-				waits(450);
-				runs(function() {
-					expect(carousel.ul.css("left")).toBe("-400px");
-				});
-			});
-		});
-	});
-
-	xdescribe("Moving the carousel forwards", function() {
-		it("Moves the carousel forwards.", function() {
-			var carousel = new kitty.Carousel($carousel);
-			carousel.moveForwards();
-			waits(450);
-			runs(function() {
-				expect(carousel.ul.css("left")).toBe("200px");
-				carousel.moveForwards();
-				waits(450);
-				runs(function() {
-					expect(carousel.ul.css("left")).toBe("400px");
-				});
-			});
-		});
-	});
-
-	describe("Destroying a carousel", function() {
-		it("Restores the carousel to it's original size", function() {
-			var ulOriginalWidth = $("div.carousel ul").width();
-			var carousel = new kitty.Carousel($carousel);
-			carousel.destroy();
-			expect($carousel.find("ul.items").width()).toBe(ulOriginalWidth);
-		});
+	describe("Creating a carousel", function() {
+        beforeEach(function () {
+            carousel = new kitty.Carousel(mockContainer);
+        })
+        it('Finds the carousel items container', function () {
+            expect(mockContainer.find).toHaveBeenCalledWith("ul.items");
+            expect(carousel.ul).toBe(mockItemsContainer);
+        });
+        it('Finds the carousel items collection', function () {
+            expect(mockItemsContainer.find).toHaveBeenCalledWith('li.item');
+            expect(carousel.lis).toBe(mockItems);
+        });
+        it('Sets the width of the items container to match the width of all the items in the carousel', function () {
+            expect(mockItemsContainer.width).toHaveBeenCalledWith(mockItems.length*mockItemsWidth);
+        });
 	});
 
 });
