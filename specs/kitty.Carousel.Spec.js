becb219528b9c8249b85c2e38e3c0f86dcad2f58
Adam Silver <adambsilver@gmail.com>
2014-08-05 14:06:05 +0100

    added carousel specs and demo

--- specs/kitty.Carousel.Spec.js
+++ specs/kitty.Carousel.Spec.js
@@ -5,35 +5,37 @@ describe("Carousel", function() {
     var mockItemsContainer;
     var mockItems;
     var mockItemsWidth;
+    var mockCssWidth;
 
 	beforeEach(function() {
 		mockContainer = jasmine.createSpyObj('mockContainer', ['find']);
-        mockItemsContainer = jasmine.createSpyObj('mockItemsContainer', ['find', 'width']);
-        mockItems = jasmine.createSpyObj('mockItem', ['width']);
-        mockItems.length = 2;
-        mockItemsWidth = 100;
-
-        mockContainer.width = jasmine.createSpy();
-
-        mockItems.width = jasmine.createSpy().and.returnValue(mockItemsWidth);
-
-        mockContainer.find = jasmine.createSpy().and.callFake(function(arg) {
+        mockContainer.find.and.callFake(function(arg) {
             if(arg === 'ul.items') {
                 return mockItemsContainer;
             }
         });
 
-        mockItemsContainer.find = jasmine.createSpy().and.callFake(function(arg) {
+        mockItemsContainer = jasmine.createSpyObj('mockItemsContainer', ['find', 'width', 'css', 'animate']);
+        mockItemsContainer.css.and.callFake(function (rule, value) {
+            if(rule === "left") {
+                return mockCssWidth;
+            }
+        });
+        mockItemsContainer.find.and.callFake(function(arg) {
             if(arg === 'li.item') {
                 return mockItems;
             }
         });
+
+        mockItems = jasmine.createSpyObj('mockItem', ['width']);
+        mockItems.width.and.returnValue(mockItemsWidth);
+        mockItems.length = 2;
+        mockItemsWidth = 100;
+        mockCssWidth = "100px";
+        carousel = new kitty.Carousel(mockContainer);
 	});
 
 	describe("Creating a carousel", function() {
-        beforeEach(function () {
-            carousel = new kitty.Carousel(mockContainer);
-        })
         it('Finds the carousel items container', function () {
             expect(mockContainer.find).toHaveBeenCalledWith("ul.items");
             expect(carousel.ul).toBe(mockItemsContainer);
@@ -47,4 +49,36 @@ describe("Carousel", function() {
         });
 	});
 
+    describe('Moving the carousel to the next item', function () {
+        beforeEach(function () {
+            carousel.moveForwards();
+        });
+        it('Retrieves the left css value', function () {
+            expect(mockItemsContainer.css).toHaveBeenCalledWith('left');
+        });
+        it('Animates the left css value by 200', function () {
+            expect(mockItemsContainer.animate).toHaveBeenCalledWith({
+               left: "300px"
+            });
+        });
+    });
+    describe('Moving the carousel to the previous item', function () {
+        beforeEach(function () {
+            carousel.moveBackwards();
+        });
+        it('Retrieves the left css value', function () {
+            expect(mockItemsContainer.css).toHaveBeenCalledWith('left');
+        });
+        it('Animates the left css value by 200', function () {
+            expect(mockItemsContainer.animate).toHaveBeenCalledWith({
+                left: "-100px"
+            });
+        });
+    });
+    describe('Destroying the carousel', function () {
+        it('Removes the width style from the items container', function () {
+            carousel.destroy();
+            expect(mockItemsContainer.css).toHaveBeenCalledWith("width", "");
+        });
+    });
 });
