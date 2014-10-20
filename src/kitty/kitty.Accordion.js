96ee70f924d56de5e609083771a623d54f95c48a
Adam Silver <adambsilver@gmail.com>
2014-10-20 15:23:35 +0100

    accordion demo and added option to make them start all collapsed

--- src/kitty/kitty.Accordion.js
+++ src/kitty/kitty.Accordion.js
@@ -1,16 +1,34 @@
-kitty.Accordion = function(container) {
+kitty.Accordion = function(container, options) {
+	this.options = this.getOptions(options);
 	this.container = container;
 	this.panels = container.find(".panel");
 	this.links = container.find("a.activator");
-	this.panels.filter(":gt(0)").css("display", "none");
-	this.links.bind("click", $.proxy(this.handleActivator_onClick, this));
-	this.currentlyOpenPanelIndex = 0;
+	this.currentlyOpenPanelIndex = null;
+	if(this.options.startCollapsed) {
+		this.panels.css("display", "none");
+	} else {
+		this.panels.filter(":gt(0)").css("display", "none");
+		this.currentlyOpenPanelIndex = 0;
+	}
+
+	this.links.on("click", $.proxy(this.onActivatorClicked, this));	
 };
-kitty.Accordion.prototype.handleActivator_onClick = function(e) {
+
+kitty.Accordion.prototype.getOptions = function(options) {
+	var options = options || {};
+	options.startCollapsed = options.startCollapsed || false;
+	return options;
+};
+
+kitty.Accordion.prototype.onActivatorClicked = function(e) {
 	e.preventDefault();
-	var link = $(e.target),
-		href = link[0].href,
-		panel;
+	var link = $(e.target);
+	this.activatePanel(link);
+};
+
+kitty.Accordion.prototype.activatePanel = function(link) {
+	var href = link[0].href;
+	var panel;
 	href = href.substr(href.indexOf("#"), href.length);
 	panel = $(href);
 	if (panel.css("display") === "none") {
@@ -23,6 +41,7 @@ kitty.Accordion.prototype.handleActivator_onClick = function(e) {
 	this.hideCurrentlyOpenPanel();
 	this.currentlyOpenPanelIndex = this.panels.index(panel);
 };
+
 kitty.Accordion.prototype.hideCurrentlyOpenPanel = function() {
 	this.panels.filter(":eq(" + this.currentlyOpenPanelIndex + ")").animate({
 		"height": "hide"
@@ -30,10 +49,10 @@ kitty.Accordion.prototype.hideCurrentlyOpenPanel = function() {
 		duration: 300
 	});
 };
+
 kitty.Accordion.prototype.destroy = function() {
-	this.panels.removeAttr("style");
-	//for(var i = 0; i < this.panels.length; i++) {
-	//this.panels[i].removeAttribute("style");
-	//}
-	this.links.unbind("click", this.handleActivator_onClick);
+	for(var i = 0; i < this.panels.length; i++) {
+		this.panels[i].removeAttribute("style");
+	}
+	this.links.unbind("click", this.onActivatorClicked);
 };
