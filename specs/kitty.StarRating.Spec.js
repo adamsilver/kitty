0ab0b5a71c16736e4ac23051e06257152e6ba0ca
Adam Silver <adam.b.silver@just-eat.com>
2015-02-25 09:28:30 +0000

    tests now have full coverage

--- specs/kitty.StarRating.Spec.js
+++ specs/kitty.StarRating.Spec.js
@@ -17,7 +17,7 @@ describe("Star Rating", function() {
 				return mockCheckedRadio;
 			}
 		});
-		mockLabels = jasmine.createSpyObj("mockLabels", ["blah"]);
+		mockLabels = jasmine.createSpyObj("mockLabels", ["filter"]);
 		mockContainer.find.and.callFake(function(selector) {
 			if(selector === ".radioControl") {
 				return mockRadioControls;
@@ -96,6 +96,152 @@ describe("Star Rating", function() {
 			it("Doesn't highlight the stars", function() {
 				expect(starRatingControl.highlightStars).not.toHaveBeenCalled();
 			});
+		});
+	});
+
+	describe("Mouse entered a label", function() {
+		var mockEvent;
+		var el;
+		beforeEach(function() {
+			mockEvent = {
+				currentTarget: {
+					htmlFor: "blah"
+				}
+			};
+			el = document.createElement("input");
+			el.value = "mockValue";
+			el.id = "blah";
+			document.body.appendChild(el);
+			starRatingControl = new kitty.StarRatingControl(mockContainer);
+			spyOn(starRatingControl, "highlightStars");
+			starRatingControl.onLabelMouseEntered(mockEvent);
+		});
+		afterEach(function() {
+			document.body.removeChild(el);
+		});
+		it("Highlights the stars based on the value of the related radio control", function() {
+			expect(starRatingControl.highlightStars).toHaveBeenCalledWith("mockValue");
+		});
+	});
+
+	describe("Mouse left a label", function() {
+		beforeEach(function() {
+			starRatingControl = new kitty.StarRatingControl(mockContainer);
+			spyOn(starRatingControl, "highlightStars");
+			starRatingControl.onLabelMouseLeft();
+		});
+		it("Highlights the stars based on the currentRating", function() {
+			expect(starRatingControl.highlightStars).toHaveBeenCalledWith(starRatingControl.currentRating);
+		});
+	});
+
+	describe("Radio focussed", function() {
+		var mockEvent;
+		var mockRadio;
+		beforeEach(function() {
+			mockEvent = {
+				target: {}
+			};
+			mockRadio = jasmine.createSpyObj("mockRadio", ["val"]);
+			mockRadio.val.and.returnValue("mockValue");
+			spyOn(window, "$").and.callFake(function(arg) {
+				if(arg == mockEvent.target) {
+					return mockRadio;
+				}
+			});
+			starRatingControl = new kitty.StarRatingControl(mockContainer);
+			spyOn(starRatingControl, "highlightStars");
+			starRatingControl.onRadioFocussed(mockEvent);
+		});
+		it("Highlights the stars based on the value of the focussed radio", function() {
+			expect(starRatingControl.highlightStars).toHaveBeenCalledWith("mockValue");
+		});
+	});
+
+	describe("Radio blurred", function() {
+		beforeEach(function() {
+			starRatingControl = new kitty.StarRatingControl(mockContainer);
+			spyOn(starRatingControl, "highlightStars");
+			starRatingControl.onRadioBlurred();
+		});
+		it("Highlights the stars based on the currentRating", function() {
+			expect(starRatingControl.highlightStars).toHaveBeenCalledWith(starRatingControl.currentRating);
+		});
+	});
+
+	describe("Radio changed", function() {
+		var mockEvent;
+		var mockRadio;
+		beforeEach(function() {
+			mockEvent = {
+				target: {}
+			};
+			mockRadio = jasmine.createSpyObj("mockRadio", ["val"]);
+			mockRadio.val.and.returnValue("mockValue");
+			spyOn(window, "$").and.callFake(function(arg) {
+				if(arg === mockEvent.target) {
+					return mockRadio;
+				}
+			});
+			starRatingControl = new kitty.StarRatingControl(mockContainer);
+			spyOn(starRatingControl, "highlightStars");
+			starRatingControl.onRadioChanged(mockEvent);
+		});
+		it("Sets the currentRating to the value of the radio", function() {
+			expect(starRatingControl.currentRating).toBe("mockValue");
+		});
+		it("Highlights the stars using the currentRating", function() {
+			expect(starRatingControl.highlightStars).toHaveBeenCalledWith(starRatingControl.currentRating);
+		});
+	});
+
+	describe("Highlighting stars", function() {
+
+
+		function createLabel() {
+			return jasmine.createSpyObj("mockLabelToHighlight", ["addClass", "removeClass"]);
+		}
+
+		var mockLabel1;
+		var mockLabel2;
+		var mockLabel3;
+		var mockLabel4;
+		var mockLabel5;
+
+		beforeEach(function() {
+			mockLabels.filter.and.callFake(function(arg) {
+				var label;
+				switch(arg) {
+					case ':eq(0)':
+						label = mockLabel1 = createLabel();
+						break;
+					case ':eq(1)':
+						label = mockLabel2 = createLabel();
+						break;
+					case ':eq(2)':
+						label = mockLabel3 = createLabel();
+						break;
+					case ':eq(3)':
+						label = mockLabel4 = createLabel();
+						break;
+					case ':eq(4)':
+						label = mockLabel5 = createLabel();
+						break;
+				}
+				return label;
+			});
+			mockLabels.length = 5;
+			starRatingControl = new kitty.StarRatingControl(mockContainer);
+			starRatingControl.highlightStars("3");
+		});
+		it("Adds a class of highlight to all labels that are equal to or below the value", function() {
+			expect(mockLabel1.addClass).toHaveBeenCalledWith("highlight");
+			expect(mockLabel2.addClass).toHaveBeenCalledWith("highlight");
+			expect(mockLabel3.addClass).toHaveBeenCalledWith("highlight");
+		});
+		it("Removes a class of highlight to all the labels that are above the rating value", function() {
+			expect(mockLabel4.removeClass).toHaveBeenCalledWith("highlight");
+			expect(mockLabel5.removeClass).toHaveBeenCalledWith("highlight");
 		});
 	});
 
