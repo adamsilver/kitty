a6852c7ee686c35910df1d853d44638eabd84995
Adam Silver <adam.b.silver@just-eat.com>
2015-03-10 09:10:07 +0000

    simplified collection, for now

--- src/kitty/kitty.Collection.js
+++ src/kitty/kitty.Collection.js
@@ -1,23 +1,9 @@
-kitty.Collection = function(options) {
+kitty.Collection = function() {
 	kitty.Collection.superConstructor.call(this);
-	this.options = options || {};
-	this.ModelConstructor = this.options.ModelConstructor || kitty.Model;
 	this.models = [];
 };
 
 kitty.inherit(kitty.Collection, kitty.EventEmitter);
-
-kitty.Collection.prototype.createModelFromAttributes = function(attributes) {
-	var model;
-	model = new this.ModelConstructor(attributes);
-	this.models.push(model);
-};
-
-kitty.Collection.prototype.createModelsFromAttributes = function(modelObjectList) {
-	for(var i = 0; i < modelObjectList.length; i++) {
-		this.createModelFromAttributes(modelObjectList[i]);
-	}
-};
 
 kitty.Collection.prototype.addModel = function(model) {
 	this.model.push(model);
