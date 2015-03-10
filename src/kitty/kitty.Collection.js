1945e1bc3c6a1b22a8931f64d1450fb324f9d2dd
Adam Silver <adam.b.silver@just-eat.com>
2015-03-10 09:09:37 +0000

    created collection to store models

--- src/kitty/kitty.Collection.js
+++ src/kitty/kitty.Collection.js
@@ -1,16 +1,30 @@
 kitty.Collection = function(options) {
 	kitty.Collection.superConstructor.call(this);
-	this.options = options;
-	this.ModelConstructor  = this.options.ModelConstructor || kitty.Model;
+	this.options = options || {};
+	this.ModelConstructor = this.options.ModelConstructor || kitty.Model;
 	this.models = [];
 };
 
 kitty.inherit(kitty.Collection, kitty.EventEmitter);
 
-kitty.Collection.prototype.addModel = function(attributes) {
-	// create a new model from these attributes
+kitty.Collection.prototype.createModelFromAttributes = function(attributes) {
+	var model;
+	model = new this.ModelConstructor(attributes);
+	this.models.push(model);
 };
 
-kitty.Collection.prototype.addModels = function(modelObjectList) {
-	// body...
+kitty.Collection.prototype.createModelsFromAttributes = function(modelObjectList) {
+	for(var i = 0; i < modelObjectList.length; i++) {
+		this.createModelFromAttributes(modelObjectList[i]);
+	}
+};
+
+kitty.Collection.prototype.addModel = function(model) {
+	this.model.push(model);
+};
+
+kitty.Collection.prototype.addModels = function(models) {
+	for(var i = 0; i < models.length; i++) {
+		this.model.push(models[i]);
+	}
 };
