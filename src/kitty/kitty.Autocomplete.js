0e878b3c7a36ceced33a9e6943772a0980cd5d95
adamsilver <adamsilver@ucds.email>
2017-05-05 09:03:31 +0100

    Clear blur timeout when clicking li perhaps?

--- src/kitty/kitty.Autocomplete.js
+++ src/kitty/kitty.Autocomplete.js
@@ -157,7 +157,7 @@ kitty.Autocomplete.prototype.onSuggestionClick = function(e) {
 	this.textBox.val($(e.currentTarget).text());
 	this.hideOptions();
 	this.focusTextBox();
-	e.preventDefault(); // for ie9 clicking scroll bar inside overflow :()
+	window.clearTimeout(this.timeout);
 };
 
 kitty.Autocomplete.prototype.onTextBoxEnterPressed = function(e) {
