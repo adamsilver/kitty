bd81fe194518d1bad8038bc02ab2cd16cab18d59
adamsilver <adamsilver@ucds.email>
2017-06-29 10:44:26 +0100

    Form validator

--- src/kitty/kitty.FormValidator.js
+++ src/kitty/kitty.FormValidator.js
@@ -1,71 +1,90 @@
-/**
- * @namespace
- */
 
-/**
- * Validates form fields within a form.
- * @constructor
- * @class
- * @param {Element} form The form node
- */
-kitty.FormValidator = function(form) {
+kitty.FormValidator = function(form, options) {
 	this.form = form;
 	this.errors = [];
 	this.validators = [];
+	$(this.form).on("submit", $.proxy(this, "onFormSubmit"));
+	this.errorSummary = $(".errorSummary");
+	this.errorSummary.on('click', 'a', $.proxy(this, 'onErrorClicked'));
 };
 
-/**
- * Adds a field to be validated against given rules
- * @param {String} fieldName The name of the field to validate
- * @param {Array[Object]} rules The rules for the validator to check against
- * @example
- * var formValidator = new kitty.FormValidator(formNode);
- * formValidator.addValidator("username", [{
- *    method: function() {
- *        // some test here and return true or false
- *        return true;
- *    },
- *    message: "This value for username is unacceptable"
- * }])
- */
+
+kitty.FormValidator.prototype.onErrorClicked = function(e) {
+    e.preventDefault();
+    var href = e.target.href;
+    href = href.substring(href.indexOf("#")+1, href.length);
+    document.getElementById(href).focus();
+};
+
+kitty.FormValidator.prototype.showErrorSummary = function () {
+    this.errorSummary.html(this.getErrorSummaryHtml());
+    this.errorSummary.removeClass('errorSummary-isHidden');
+    this.errorSummary.focus();
+};
+
+kitty.FormValidator.prototype.getErrorSummaryHtml = function() {
+	var errors = this.getErrors();
+    var html = '<h2>You have ' + errors.length + ' errors</h2>';
+    html += '<ul>';
+    for (var i = 0, j = errors.length; i < j; i++) {
+        var error = errors[i];
+        html += '<li>';
+        html +=		'<a href="#' + error.fieldName + '">';
+        html +=			error.message;
+        html +=		'</a>';
+        html +=	'</li>';
+    }
+    html += '</ul>';
+    return html;
+};
+
+kitty.FormValidator.prototype.hideErrorSummary = function() {
+    this.errorSummary.addClass('errorSummary-isHidden');
+};
+
+kitty.FormValidator.prototype.onFormSubmit = function (e) {
+	this.clearFieldErrors();
+	this.hideErrorSummary();
+	if(!this.validate()) {
+		e.preventDefault();
+		this.showErrorSummary();
+		this.showFieldErrors();
+	}
+};
+
+kitty.FormValidator.prototype.showFieldErrors = function() {
+	var errors = this.getErrors();
+	for (var i = 0, j = errors.length; i < j; i++) {
+		this.showFieldError(errors[i]);
+	}
+};
+
+kitty.FormValidator.prototype.showFieldError = function (error) {
+	var errorSpan = '<span class="error"><span>Error:</span> '+error.message+'</span>';
+	var fieldContainer = $("#" + error.fieldName).parents(".field");
+	var label = fieldContainer.find('label');
+	var legend = fieldContainer.find("legend");
+	var errorContainer = fieldContainer.find(".error");
+	errorContainer.remove();
+	if(legend.length) {
+		legend.append(errorSpan);
+	} else {
+		label.append(errorSpan);
+	}
+};
+
+kitty.FormValidator.prototype.clearFieldErrors = function () {
+	$(this.form).find(".field .error").remove();
+};
+
 kitty.FormValidator.prototype.addValidator = function(fieldName, rules) {
-	var exceptionMessageRules = "Invalid rules. Must provide be an array of rules (at least 1).",
-		field = this.form.elements[fieldName],
-		rule,
-		i;
-	// if field does not exist
-	if (!field) {
-		throw "Invalid form field.";
-	}
-	if (!rules) {
-		throw exceptionMessageRules;
-	}
-	if (!rules.length) {
-		throw exceptionMessageRules;
-	}
-	if (rules.length) {
-		for (i = 0; i < rules.length; i++) {
-			rule = rules[i];
-			if (typeof rule.method !== "function" ||
-				typeof rule.message !== "string") {
-				throw exceptionMessageRules;
-			}
-		}
-	}
 	this.validators.push({
 		fieldName: fieldName,
 		rules: rules,
-		field: field
+		field: this.form.elements[fieldName]
 	});
 };
 
-/**
- * Iterates through the list of fields to validate
- * @return {Boolean} Returns false if there are errors, otherwise true
- * @example
- * var formValidator = new kitty.FormValidator(formNode);
- * formValidator.validate();
- */
 kitty.FormValidator.prototype.validate = function() {
 	this.errors = [];
 	var validator = null,
@@ -86,57 +105,42 @@ kitty.FormValidator.prototype.validate = function() {
 			}
 		}
 	}
-	return this.errors.length === 0;
+	return this.getErrors().length === 0;
 };
 
-/**
- * Retrieve a list of errors. Typically called after determining that validate
- * returned false
- * @return {Array[Object]} List of errors in format { fieldName: "",
- * message: "" }
- */
 kitty.FormValidator.prototype.getErrors = function() {
 	return this.errors;
 };
 
-/**
- * Remove a validator from the form validator
- * @param  {String} fieldName The name of the field to remove from validation
- */
-kitty.FormValidator.prototype.removeValidator = function(fieldName) {
-	var i;
-	for (i = 0; i < this.validators.length; i++) {
-		if (this.validators[i].fieldName === fieldName) {
-			this.validators.splice(i, 1);
-			break;
-		}
-	}
-};
+// kitty.FormValidator.prototype.removeValidator = function(fieldName) {
+// 	var i;
+// 	for (i = 0; i < this.validators.length; i++) {
+// 		if (this.validators[i].fieldName === fieldName) {
+// 			this.validators.splice(i, 1);
+// 			break;
+// 		}
+// 	}
+// };
 
-/**
- * Remove a single rule from a validator
- * @param  {String} fieldName The name of the field
- * @param  {Function} ruleFunction The function reference for the rule
- */
-kitty.FormValidator.prototype.removeRuleFromValidator = function(fieldName,
-	ruleFunction) {
-	var validator = null,
-		rules,
-		rule,
-		i,
-		j;
-	for (i = 0; i < this.validators.length; i++) {
-		validator = this.validators[i];
-		if (validator.fieldName === fieldName) {
-			rules = validator.rules;
-			for (j = 0; j < rules.length; j++) {
-				rule = rules[j];
-				if (rule.method === ruleFunction) {
-					rules.splice(j, 1);
-					break;
-				}
-			}
-			break;
-		}
-	}
-};
+// kitty.FormValidator.prototype.removeRuleFromValidator = function(fieldName,
+// 	ruleFunction) {
+// 	var validator = null,
+// 		rules,
+// 		rule,
+// 		i,
+// 		j;
+// 	for (i = 0; i < this.validators.length; i++) {
+// 		validator = this.validators[i];
+// 		if (validator.fieldName === fieldName) {
+// 			rules = validator.rules;
+// 			for (j = 0; j < rules.length; j++) {
+// 				rule = rules[j];
+// 				if (rule.method === ruleFunction) {
+// 					rules.splice(j, 1);
+// 					break;
+// 				}
+// 			}
+// 			break;
+// 		}
+// 	}
+// };
