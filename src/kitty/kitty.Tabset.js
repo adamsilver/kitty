8c5f277adfdf3a449c9c5677ce3a013463a6dc71
Adam Silver <adambsilver@gmail.com>
2018-04-10 19:35:09 +0100

    Tabs

--- src/kitty/kitty.Tabset.js
+++ src/kitty/kitty.Tabset.js
@@ -1,26 +1,98 @@
 kitty.Tabset = function(container) {
+	this.container = container;
+	 this.keys = {
+		left: 37,
+		right: 39
+   };
 	this.cssActive = "active";
-	this.cssHide = "hide";
-	this.links = container.find("a.tabActivator");
-	this.panels = container.find("div.panel");
-	this.panels.addClass("hide");
-	container.find("div.panel:first").removeClass(this.cssHide);
-	container.find("a.tabActivator:first").addClass(this.cssActive);
-	this.links.bind("click", $.proxy(this.handleLink_onClick, this));
+	this.cssHide = "hidden";
+	this.links = container.find("> ul a");
+	this.panels = container.find(".tabs__panel");
+	this.panels.addClass("hidden");
+	container.find(".tabs__panel:first").removeClass(this.cssHide);
+	
+	this.links.on("click", $.proxy(this, 'onTabClick'));
+	this.links.on('keydown', $.proxy(this, 'onTabKeydown'));
+
+	// enhance markup
+	container.find('> ul').attr('role', 'tablist');
+	this.links.attr('role', 'tab');
+	this.links.attr('tabindex', '-1');
+  $('.tabs > ul li').attr('role', 'presentation');
+	this.panels.attr('role', 'tabpanel');
+
+	container.find("> ul a:first")
+		.attr('aria-selected', 'true')
+		.attr('tabindex', '0');
 };
-kitty.Tabset.prototype.handleLink_onClick = function(e) {
+
+kitty.Tabset.prototype.onTabClick = function(e) {
 	e.preventDefault();
-	var link = $(e.target);
-	var panel = $(this.getHref(link));
-	this.panels.addClass(this.cssHide);
-	this.links.removeClass(this.cssActive);
-	panel.removeClass(this.cssHide);
-	link.addClass(this.cssActive);
+	var newTab = $(e.target);
+	var currentTab = this.getCurrentTab();
+	this.unhighlightTab(currentTab);
+	this.hidePanel(currentTab);
+	this.highlightTab(newTab);
+	this.showPanel(newTab);
 };
-kitty.Tabset.prototype.destroy = function() {
-	this.panels.removeClass(this.cssHide);
-	this.links.unbind("click", this.handleLink_onClick);
+
+kitty.Tabset.prototype.onTabKeydown = function(e) {
+	switch(e.keyCode) {
+		case this.keys.left:
+			this.activatePreviousTab();
+			break;
+		case this.keys.right:
+			this.activateNextTab();
+			break;
+	}
 };
+
+kitty.Tabset.prototype.activateNextTab = function() {
+	var currentTab = this.getCurrentTab();
+	var nextTab = currentTab.parent().next().find('[role=tab]');
+	if(nextTab[0]) {
+		this.unhighlightTab(currentTab);
+		this.highlightTab(nextTab);
+		this.hidePanel(currentTab);
+		this.showPanel(nextTab);
+		nextTab.focus();
+	}
+};
+
+kitty.Tabset.prototype.activatePreviousTab = function() {
+	var currentTab = this.getCurrentTab();
+	var previousTab = currentTab.parent().prev().find('[role=tab]');
+	if(previousTab[0]) {
+		this.unhighlightTab(currentTab);
+		this.highlightTab(previousTab);
+		this.hidePanel(currentTab);
+		this.showPanel(previousTab);
+		previousTab.focus();
+	}
+};
+
+kitty.Tabset.prototype.showPanel = function(tab) {
+	$(this.getHref(tab)).removeClass(this.cssHide);
+};
+
+kitty.Tabset.prototype.hidePanel = function(tab) {
+	$(this.getHref(tab)).addClass(this.cssHide);
+};
+
+kitty.Tabset.prototype.unhighlightTab = function(tab) {
+	tab.attr('aria-selected', 'false');
+	tab.attr('tabindex', '-1');
+};
+
+kitty.Tabset.prototype.highlightTab = function(tab) {
+	tab.attr('aria-selected', 'true');
+	tab.attr('tabindex', '0');
+};
+
+kitty.Tabset.prototype.getCurrentTab = function() {
+	return this.container.find("[role=tab][aria-selected=true]");
+};
+
 // this is because IE doesn't always return the actual value but a relative full path
 // http://labs.thesedays.com/blog/2010/01/08/getting-the-href-value-with-jquery-in-ie/
 kitty.Tabset.prototype.getHref = function(link) {
