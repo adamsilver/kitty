49fd7b6fca5f5d5ad3d3e0ca696a6770a67420e7
Adam Silver <adambsilver@gmail.com>
2018-04-15 15:56:59 +0100

    So far

--- src/kitty/kitty.Tabs.js
+++ src/kitty/kitty.Tabs.js
@@ -9,14 +9,42 @@ kitty.Tabs = function(container) {
 	// events
 	this.links.on("click", $.proxy(this, 'onTabClick'));
 	this.links.on('keydown', $.proxy(this, 'onTabKeydown'));
+	$(window).on('popstate', $.proxy(this, 'onPopState'));
 
 	this.setupHtml();
 
 	// setup state
 	this.links.attr('tabindex', '-1');
 	this.panels.addClass("hidden");
-	this.highlightTab(this.links.first());
-	this.showPanel(this.links.first());
+
+	// if there's a tab that matches the hash
+	var tab = this.getTab(window.location.hash);
+	if(tab.length) {
+		this.highlightTab(tab);
+		this.showPanel(tab);
+		history.replaceState(this.getHref(tab), null, this.getHref(tab));
+	} else {
+		// or show the first
+		var firstTab = this.links.first();
+		this.highlightTab(firstTab);
+		this.showPanel(firstTab);
+		history.replaceState(this.getHref(firstTab), null, '/blah');
+	}
+};
+
+kitty.Tabs.prototype.onPopState = function(e) {
+	var tab = this.getTab(window.location.hash);
+	if(tab.length) {
+		var currentTab = this.getCurrentTab();
+		this.unhighlightTab(currentTab);
+		this.hidePanel(currentTab);
+		this.highlightTab(tab);
+		this.showPanel(tab);
+	}
+};
+
+kitty.Tabs.prototype.getTab = function(hash) {
+	return this.links.filter('a[href="' + hash +'"]');
 };
 
 kitty.Tabs.prototype.setupHtml = function() {
@@ -34,6 +62,7 @@ kitty.Tabs.prototype.onTabClick = function(e) {
 	this.hidePanel(currentTab);
 	this.highlightTab(newTab);
 	this.showPanel(newTab);
+	history.pushState(this.getHref(newTab), null, this.getHref(newTab));
 };
 
 kitty.Tabs.prototype.onTabKeydown = function(e) {
@@ -52,9 +81,10 @@ kitty.Tabs.prototype.activateNextTab = function() {
 	var nextTab = currentTab.parent().next().find('[role=tab]');
 	if(nextTab[0]) {
 		this.unhighlightTab(currentTab);
-		this.highlightTab(nextTab);
 		this.hidePanel(currentTab);
+		this.highlightTab(nextTab);
 		this.showPanel(nextTab);
+		history.pushState(this.getHref(nextTab), null, this.getHref(nextTab));
 		nextTab.focus();
 	}
 };
@@ -64,9 +94,10 @@ kitty.Tabs.prototype.activatePreviousTab = function() {
 	var previousTab = currentTab.parent().prev().find('[role=tab]');
 	if(previousTab[0]) {
 		this.unhighlightTab(currentTab);
-		this.highlightTab(previousTab);
 		this.hidePanel(currentTab);
+		this.highlightTab(previousTab);
 		this.showPanel(previousTab);
+		history.pushState(this.getHref(previousTab), null, this.getHref(previousTab));
 		previousTab.focus();
 	}
 };
